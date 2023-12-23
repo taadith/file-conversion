a6ea427525313a543923ee50ce8b02c1ddfea1ad
Aadith_Th <taadith@gmail.com>
2023-12-23 11:45:08 -0600

    changed print_file_info() and added print_chunk_info() for png-file/

--- png-file/read-file.c
+++ png-file/read-file.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void file_length(FILE *fp, long *fl) {
     // moves file access position to EOF
@@ -13,12 +14,30 @@ void file_length(FILE *fp, long *fl) {
     rewind(fp);
 }
 
-void print_file_header_contents(FILE *fp) {
+void print_png_chunk_information(unsigned char *chunk_start) {
+    int chunk_data_length = 0, chunk_length = 12;
+    char chunk_type[5]; chunk_type[4] = '\0';
+    
+    for(int i = 0; i < 4; i++)
+        chunk_data_length += chunk_start[i] << (8 * (3 - i));
+    chunk_length += chunk_data_length;
+    
+    char ancillary, private, reserved, safe_to_copy;
+    strncpy(chunk_type, (const char *)(chunk_start + 4), 4);
+    
+    
+    printf("Chunk Type: %s\n", chunk_type);
+    printf("Total Length of Chunk: %d\n", chunk_length);
+    printf("Length of Data: %d\n", chunk_data_length);
+}
+
+void print_png_file_information(FILE *fp, char *filename) {
     if (fp == NULL) {
-        printf("error: file pointer is null\n");
-        exit(1);
+        perror("error: file pointer is null\n");
+        return;
     }
     else {
+        printf("File Name: %s\n", filename);
         long fl = 0;
         file_length(fp,&fl);
         printf("File Size: %ld bytes\n", fl);
@@ -48,24 +67,24 @@ void print_file_header_contents(FILE *fp) {
         printf("Filter Method: %d, value indicates preprocessing method applied to image data before compression\n", filter_method);
         printf("Interlace Method: %d, value determines the transmission order of the image data\n", interlace_method);
 
+        print_png_chunk_information(file_contents + 33);
+        print_png_chunk_information(file_contents + 33 + 226888);
+
         free(file_contents);
     }
 }
 
+
+
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        FILE *fp = fopen("./dice.png", "rb");
-        print_file_header_contents(fp);
-        fclose(fp);
-    }
-    else if (argc == 2) {
+    if (argc == 2) {
         FILE *fp = fopen(argv[1], "rb");
-        print_file_header_contents(fp);
+        print_png_file_information(fp, argv[1]);
         fclose(fp);
     }
     else {
-        printf("error: invalid # of arguments passed to png-rf");
-        exit(1);
+        perror("error: invalid # of arguments passed to png-rf");
+        return (1);
     }
 
     return 0;
